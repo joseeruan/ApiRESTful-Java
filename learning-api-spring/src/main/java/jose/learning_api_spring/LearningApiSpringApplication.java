d61cf21cfb26ae6b37cd6a85ec516c29831c0075
José Ruan <jrpaivadev@gmail.com>
2025-02-19 04:07:24 -0300

    Initial commit

---
+++ learning-api-spring/src/main/java/jose/learning_api_spring/LearningApiSpringApplication.java
@@ -0,0 +1,13 @@
+package jose.learning_api_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningApiSpringApplication.class, args);
+	}
+
+}
---
+++ learning-api-spring/src/test/java/jose/learning_api_spring/LearningApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package jose.learning_api_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
